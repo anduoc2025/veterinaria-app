9b91e005c4b059d4836df5d166a70646d45ec4ee
anduoc2025 <an.barra@duocuc.cl>
2025-11-10 11:53:23 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PetControlNG',
+  webDir: 'www'
+};
+
+export default config;
